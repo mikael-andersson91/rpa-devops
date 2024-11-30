a9043eb9cac0893b4958fef0d13742c14f994e70
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2024-11-30 10:56:41 +0100

    feat(components): add Secrets to workflow pages
    Automatically generate table of secrets passed to a GitHub Actions workflow
    on documentation pages

--- src/components/GitHubActionsWorkflow/WorkflowSecrets/index.tsx
+++ src/components/GitHubActionsWorkflow/WorkflowSecrets/index.tsx
@@ -1,12 +1,45 @@
+import React from 'react';
 
-export class WorkflowSecret {
-    name: string;
-    required: boolean;
-    description: string;
-
-    constructor(name: string, required: boolean, description: string) {
-        this.name = name;
-        this.required = required;
-        this.description = description;
-    }
+interface WorkflowSecret {
+    secret?: string;
+    description?: string;
 }
+
+interface WorkflowSecrets {
+    workflow: string;
+}
+
+const WorkflowSecretList: React.FC<WorkflowSecrets> = (props) => {
+    const wf = require('yaml-loader!@site/.github/workflows/' + props.workflow);
+    const workflowCall = wf?.default?.on?.workflow_call;
+    const secrets: { [key: string]: WorkflowSecret } | undefined = workflowCall?.secrets;
+
+    if (secrets == undefined) {
+        return <p>There are no secrets defined in this workflow</p>;
+    } else {
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Secret</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(secrets).map((value, idx) => {
+                        return (
+                            <tr key={idx}>
+                                <td className="secret">{value}</td>
+                                <td className="secretDesc">
+                                    {secrets[value].description || 'missing description'}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+};
+
+export default WorkflowSecretList;
